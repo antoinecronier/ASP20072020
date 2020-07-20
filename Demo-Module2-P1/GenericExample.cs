8afef216400998affc1caa44766814370e36b7bd
Antoine CRONIER <acronier@campus-eni.fr>
2020-07-20 11:53:46 +0200

    Demo module2-p1

---
+++ Demo-Module2-P1/GenericExample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Module2_P1
+{
+    public class GenericExample<T, K, L > where T : new() where K : User
+    {
+        public T DoT()
+        {
+            return new T();
+        }
+
+        public void GetOtherT(T item)
+        {
+            Console.WriteLine(item);
+        }
+
+        //public T SpecialTKL(K kItem, L lItem)
+        //{
+        //}
+
+        //public M DoSMT<M>()
+        //{
+
+        //}
+    }
+}
---
+++ Demo-Module2-P1/User.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Module2_P1
+{
+    public class User : UserInterface
+    {
+        #region Java
+        private string name;
+
+        /// <summary>
+        /// Java name mutator.
+        /// </summary>
+        /// <param name="name"></param>
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        /// <summary>
+        /// Java name accessor.
+        /// </summary>
+        /// <returns></returns>
+        public string getName()
+        {
+            return this.name;
+        }
+        #endregion
+
+        #region C#
+        #region PropFull
+        private string name1;
+
+        public string Name1
+        {
+            get { return name1; }
+            set { name1 = value; }
+        }
+
+        private string name2;
+
+        public string Name2
+        {
+            get
+            {
+                if (name2.Length > 10)
+                {
+                    return "test";
+                }
+                return name2;
+            }
+            set
+            {
+                if (value.Equals("test"))
+                {
+
+                }
+                name2 = value;
+            }
+        }
+        #endregion
+        #region Auto property
+        public string Name3 { get; set; }
+        #endregion
+        #region Implemented property
+        private string password;
+        public string Password { get => this.password; set => this.password = value; }
+        #endregion
+        #endregion
+    }
+}
