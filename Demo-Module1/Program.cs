e951a774f80f873a7977b087840f8286a1eb12b7
Antoine CRONIER <acronier@campus-eni.fr>
2020-07-20 10:15:37 +0200

    Demo module1

---
+++ Demo-Module1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.WriteLine(args[0]);
+            Console.ReadKey();
+        }
+    }
+}
