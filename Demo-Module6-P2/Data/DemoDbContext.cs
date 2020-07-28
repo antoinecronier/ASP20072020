e419691d9a0c98a4bd5e8036d73a1d325cc916a9
antoine cronier <acronier@campus-eni.fr>
2020-07-28 11:34:20 +0200

    Demo fluent 3

--- Demo-Module6-P2/Data/DemoDbContext.cs
+++ Demo-Module6-P2/Data/DemoDbContext.cs
@@ -14,9 +14,17 @@ namespace Demo_Module6_P2.Data
         public DbSet<Trajet> Trajets { get; set; }
         public DbSet<Conducteur> Conducteurs { get; set; }
 
+        public DemoDbContext()
+        {
+
+        }
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Voiture>().HasOptional(x => x.Driver).WithOptionalPrincipal(x => x.Car);
+            modelBuilder.Entity<Voiture>().HasMany(x => x.CommonRides).WithOptional(x => x.CommonCar);
+            modelBuilder.Entity<Voiture>().HasMany(x => x.Rides).WithMany(x => x.Cars);
+            //modelBuilder.Entity<Trajet>().HasMany(x => x.Cars).WithMany(x => x.Rides);
         }
     }
 }
