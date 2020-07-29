d7cceb1d5a6233359d406e7ab44dda185bdf9f22
antoine cronier <acronier@campus-eni.fr>
2020-07-29 12:45:56 +0200

    Tp dojo part 2 refactoring

--- TPModule6-2/Controllers/ArmesController.cs
+++ TPModule6-2/Controllers/ArmesController.cs
@@ -11,125 +11,139 @@ using TPModule6_2.Data;
 
 namespace TPModule6_2.Controllers
 {
-    public class ArmesController : Controller
+    public class ArmesController : BaseMVCController<Arme, Arme>
     {
-        private TPModule6_2Context db = new TPModule6_2Context();
-
-        // GET: Armes
-        public ActionResult Index()
+        protected override bool DeleteRejected(int? id)
         {
-            return View(db.Armes.ToList());
-        }
-
-        // GET: Armes/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Arme arme = db.Armes.Find(id);
-            if (arme == null)
-            {
-                return HttpNotFound();
-            }
-            return View(arme);
-        }
-
-        // GET: Armes/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Armes/Create
-        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
-        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Nom,Degats")] Arme arme)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Armes.Add(arme);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(arme);
-        }
-
-        // GET: Armes/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Arme arme = db.Armes.Find(id);
-            if (arme == null)
-            {
-                return HttpNotFound();
-            }
-            return View(arme);
-        }
-
-        // POST: Armes/Edit/5
-        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
-        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Nom,Degats")] Arme arme)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(arme).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(arme);
-        }
-
-        // GET: Armes/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
+            bool result = false;
 
             List<int> ids = db.Samourais.Include(x => x.Arme).Where(x => x.Arme != null).Select(x => x.Arme.Id).ToList();
             if (ids.Contains(id.Value))
             {
                 ModelState.AddModelError("", "Cette arme ne peut pas être supprimé parce qu'elle appartient au samourai \"" + db.Samourais.FirstOrDefault(x => x.Arme.Id == id.Value).Nom + "\"");
-                return View();
+                result = true;
             }
-            Arme arme = db.Armes.Find(id);
-            if (arme == null)
-            {
-                return HttpNotFound();
-            }
-            return View(arme);
+
+            return result;
         }
 
-        // POST: Armes/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Arme arme = db.Armes.Find(id);
-            db.Armes.Remove(arme);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+        //private TPModule6_2Context db = new TPModule6_2Context();
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+        //// GET: Armes
+        //public ActionResult Index()
+        //{
+        //    return View(db.Armes.ToList());
+        //}
+
+        //// GET: Armes/Details/5
+        //public ActionResult Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Arme arme = db.Armes.Find(id);
+        //    if (arme == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(arme);
+        //}
+
+        //// GET: Armes/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: Armes/Create
+        //// Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        //// plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include = "Id,Nom,Degats")] Arme arme)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Armes.Add(arme);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View(arme);
+        //}
+
+        //// GET: Armes/Edit/5
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Arme arme = db.Armes.Find(id);
+        //    if (arme == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(arme);
+        //}
+
+        //// POST: Armes/Edit/5
+        //// Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        //// plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "Id,Nom,Degats")] Arme arme)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(arme).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(arme);
+        //}
+
+        //// GET: Armes/Delete/5
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+
+        //    List<int> ids = db.Samourais.Include(x => x.Arme).Where(x => x.Arme != null).Select(x => x.Arme.Id).ToList();
+        //    if (ids.Contains(id.Value))
+        //    {
+        //        ModelState.AddModelError("", "Cette arme ne peut pas être supprimé parce qu'elle appartient au samourai \"" + db.Samourais.FirstOrDefault(x => x.Arme.Id == id.Value).Nom + "\"");
+        //        return View();
+        //    }
+        //    Arme arme = db.Armes.Find(id);
+        //    if (arme == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(arme);
+        //}
+
+        //// POST: Armes/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    Arme arme = db.Armes.Find(id);
+        //    db.Armes.Remove(arme);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
     }
 }
